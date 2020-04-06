88ba28a1e683a1226aa71ed5f181bbfed3bc039a
sam wang <wang3193@hotmail.com>
2020-04-07 07:36:50 +0800

    add contextpath

--- src/util/global.js
+++ src/util/global.js
@@ -1,6 +1,9 @@
 //配置全局参数，通过Vue.prototype.global = global // 挂载全局配置模块
 const baseUrl = "http://localhost:8080"
 
+const contxtPath = "/api"
+
 export default {
-    baseUrl
+    baseUrl,
+    contxtPath
 }
